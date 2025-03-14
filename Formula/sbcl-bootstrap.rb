369f6da1d638c503e0593104aa3e963447ef24cc
Ethan <ethan.hawk@valpo.edu>
2025-03-13 22:42:59 -0400

    sbcl-boostrap.rb: add

---
+++ Formula/sbcl-bootstrap.rb
@@ -0,0 +1,60 @@
+class SbclBoostrap < Formula
+  desc "Steel Bank Common Lisp system"
+  homepage "https://www.sbcl.org/"
+  url "https://downloads.sourceforge.net/project/sbcl/sbcl/2.5.2/sbcl-2.5.2-source.tar.bz2"
+  sha256 "5dc27eba7dda433df53fd7441de8b11474a82cdac1689c1f6ce55fa065d65fac"
+  license all_of: [:public_domain, "MIT", "Xerox", "BSD-3-Clause"]
+  head "https://git.code.sf.net/p/sbcl/sbcl.git", branch: "master"
+
+  livecheck do
+    url "https://sourceforge.net/projects/sbcl/rss?path=/sbcl"
+  end
+
+  depends_on "ecl" => :build
+  depends_on "zstd"
+
+  def install
+    # Remove non-ASCII values from environment as they cause build failures
+    # More information: https://bugs.gentoo.org/show_bug.cgi?id=174702
+    ENV.delete_if do |_, value|
+      ascii_val = value.dup
+      ascii_val.force_encoding("ASCII-8BIT") if ascii_val.respond_to? :force_encoding
+      ascii_val =~ /[\x80-\xff]/n
+    end
+
+    xc_cmdline = "ecl --norc"
+
+    args = [
+      "--prefix=#{prefix}",
+      "--xc-host=#{xc_cmdline}",
+      "--with-sb-core-compression",
+      "--with-sb-ldb",
+      "--with-sb-thread",
+    ]
+
+    ENV["SBCL_MACOSX_VERSION_MIN"] = MacOS.version.to_s if OS.mac?
+    system "./make.sh", *args
+
+    ENV["INSTALL_ROOT"] = prefix
+    system "sh", "install.sh"
+
+    # Install sources
+    bin.env_script_all_files libexec/"bin",
+                             SBCL_SOURCE_ROOT: pkgshare/"src",
+                             SBCL_HOME:        lib/"sbcl"
+    pkgshare.install %w[contrib src]
+    (lib/"sbcl/sbclrc").write <<~LISP
+      (setf (logical-pathname-translations "SYS")
+        '(("SYS:SRC;**;*.*.*" #p"#{pkgshare}/src/**/*.*")
+          ("SYS:CONTRIB;**;*.*.*" #p"#{pkgshare}/contrib/**/*.*")))
+    LISP
+  end
+
+  test do
+    (testpath/"simple.sbcl").write <<~LISP
+      (write-line (write-to-string (+ 2 2)))
+    LISP
+    output = shell_output("#{bin}/sbcl --script #{testpath}/simple.sbcl")
+    assert_equal "4", output.strip
+  end
+end
